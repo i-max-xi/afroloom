0d75e777f02eec1234d660b08e03613b07f414dd
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2023-06-20 22:01:55 +0000

    carousel

--- src/Components/HeaderCarousel2.js
+++ src/Components/HeaderCarousel2.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Carousel } from "primereact/carousel";
-import s1 from "../Assets/carousel/s1.jpg";
-import s2 from "../Assets/carousel/s2.jpg";
-import s3 from "../Assets/carousel/s3.jpg";
-import s4 from "../Assets/carousel/s4.jpg";
-import s5 from "../Assets/carousel/s5.jpg";
-import s6 from "../Assets/carousel/s6.jpg";
-import s7 from "../Assets/carousel/s7.jpg";
-import s8 from "../Assets/carousel/s8.jpg";
+// import s1 from "../Assets/carousel/s1.jpg";
+// import s2 from "../Assets/carousel/s2.jpg";
+// import s3 from "../Assets/carousel/s3.jpg";
+// import s4 from "../Assets/carousel/s4.jpg";
+// import s5 from "../Assets/carousel/s5.jpg";
+// import s6 from "../Assets/carousel/s6.jpg";
+// import s7 from "../Assets/carousel/s7.jpg";
+// import s8 from "../Assets/carousel/s8.jpg";
 
 const HeaderCarousel2 = () => {
   const products = [
