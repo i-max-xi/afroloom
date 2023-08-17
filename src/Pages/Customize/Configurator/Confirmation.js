0cc758013522a2a76c8ac3acf336bffbfbed0d17
Appiah Gyimah Maxwell <maxappiahofficial@gmail.com>
2023-08-17 14:36:56 +0000

    addded toasts

--- src/Pages/Customize/Configurator/Confirmation.js
+++ src/Pages/Customize/Configurator/Confirmation.js
@@ -1,9 +1,16 @@
 import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import html2canvas from "html2canvas";
+import { Link } from "react-router-dom";
 
 import { app } from "../../../firebase"; // Import your firebase app object
-import { getStorage, ref, uploadString, getDownloadURL } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadString,
+  getDownloadURL,
+} from "firebase/storage";
+import { Toast } from "primereact/toast";
 
 const Confirmation = ({
   price,
@@ -12,7 +19,7 @@ const Confirmation = ({
   selectedParts, // Array containing selected parts with their color and texture information
   selectedSize, // The selected size
 }) => {
-  
+  const toast = useRef(null); // Create a ref for the Toast component
 
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
@@ -21,29 +28,26 @@ const Confirmation = ({
 
   const storage = getStorage(app); // Initialize Firebase Storage with your app
 
-
   const handleFormSubmit = async () => {
     try {
       // ... (existing code)
-  
+
       // Capture the component as an image using html2canvas
       const image = await html2canvas(componentRef.current, {
         useCORS: true, // Ensure cross-origin images are captured
       });
-  
+
       // Convert the captured image into a data URL
       const imageDataURL = image.toDataURL("image/png");
-  
 
-    // Upload the captured image to Firebase Storage
-    const storageRef = ref(storage, `Order_images/${Date.now()}.png`);
-    await uploadString(storageRef, imageDataURL, "data_url");
+      // Upload the captured image to Firebase Storage
+      const storageRef = ref(storage, `Order_images/${Date.now()}.png`);
+      await uploadString(storageRef, imageDataURL, "data_url");
 
-    // Get the download URL of the uploaded image
-    const downloadURL = await getDownloadURL(storageRef);
+      // Get the download URL of the uploaded image
+      const downloadURL = await getDownloadURL(storageRef);
 
-  
-      // Create formData 
+      // Create formData
       const formData = {
         price,
         estimatedShippingTime,
@@ -53,8 +57,6 @@ const Confirmation = ({
         message: "Please use the link to access the client's order", // Add the message
       };
 
-
-  
       // Send the formData to Formspree
       const response = await fetch("https://formspree.io/f/xrgwpakw", {
         method: "POST",
@@ -63,23 +65,44 @@ const Confirmation = ({
         },
         body: JSON.stringify(formData),
       });
-  
+
       if (response.ok) {
-        alert("Order confirmed. Thank you!");
+        toast.current.show({
+          severity: "info",
+          summary: "Order Confirmed",
+          detail: (
+            <div>
+              <p>Thank you for your order!</p>
+              <p>
+                Proceed to <Link to="/checkout" className="btn btn-success">Checkout</Link>
+              </p>
+            </div>
+          ),
+          life: 7000, // Duration in milliseconds
+        });
       } else {
-        alert("Failed to confirm order. Please try again.");
+        toast.current.show({
+          severity: "error",
+          summary: "Order Confirmation Failed",
+          detail: "Failed to confirm order. Please try again.",
+          life: 3000,
+        });
       }
     } catch (error) {
-      console.error("Error submitting the form:", error);
-      alert("An error occurred while confirming the order. Please try again later.");
+      toast.current.show({
+        severity: "error",
+        summary: "Error",
+        detail:
+          "An error occurred while confirming the order. Please try again later.",
+        life: 3000,
+      });
     }
   };
-  
 
   return (
     <div className="container confirmation-page">
+      <Toast ref={toast} /> {/* Add the Toast component here */}
       <h1 className="mt-4">Order Confirmation</h1>
-
       <OrderDetail
         price={price}
         estimatedShippingTime={estimatedShippingTime}
@@ -93,14 +116,14 @@ const Confirmation = ({
           <button className="btn btn-outline-success" onClick={handlePrint}>
             Download Copy
           </button>
-          <button className="btn btn-success" onClick={handleFormSubmit}>
+          <button className="btn btn-success mx-3" onClick={handleFormSubmit}>
             Confirm Order
           </button>
         </div>
 
         <p className="h5 mt-4">Thank you for your order!</p>
 
-         {/* Display the captured image */}
+        {/* Display the captured image */}
         {/* <img
       src={capturedImageURL}
           alt="Captured Order"
@@ -111,64 +134,73 @@ const Confirmation = ({
   );
 };
 
-export const OrderDetail = React.forwardRef(({ price,
-  estimatedShippingTime,
-  readyBy,
-  selectedParts,
-  selectedSize }, ref) => {
-  return (
-    <div ref={ref} className="row">
-    <div className="col-md-6">
-      <p className="h5 mt-3 MB-5">CONVERTED 3D INTO IMAGE WILL BE HERE...</p>
-      <p className="h5 mt-4">Selected Size: {selectedSize || "N/A"}</p>
-      <p className="h5 mt-3">Price: ${price}</p>
-      <p>Estimated Shipping Time: {estimatedShippingTime}</p>
-      <p>Expected to be Ready By: {readyBy}</p>
-    </div>
-    <div className="col-md-6">
-      <div className="mt-4">
-        <h2>Information On Parts</h2>
-        {selectedParts.map((part, index) => (
-          <div key={index} className="mb-4">
-            <h5>{part.name}</h5>
-            <p>
-              Color:{" "}
-              {part.color ? (
-                <div
-                  className="color-display"
-                  style={{
-                    backgroundColor: part.color,
-                    width: "20px",
-                    height: "20px",
-                    display: "inline-block",
-                    marginLeft: "2rem"
-                  }}
-                ></div>
-              ) : (
-                <span>N/A</span>
-              )}
-            </p>
+export const OrderDetail = React.forwardRef(
+  (
+    { price, estimatedShippingTime, readyBy, selectedParts, selectedSize },
+    ref
+  ) => {
+    return (
+      <div ref={ref} className="row">
+        <div className="col-md-6">
+          <p className="h5 mt-3 MB-5">
+            CONVERTED 3D INTO IMAGE WILL BE HERE...
+          </p>
+          <p className="h5 mt-4">Selected Size: {selectedSize || "N/A"}</p>
+          <p className="h5 mt-3">Price: ${price}</p>
+          <p>Estimated Shipping Time: {estimatedShippingTime}</p>
+          <p>Expected to be Ready By: {readyBy}</p>
+        </div>
+        <div className="col-md-6">
+          <div className="mt-4">
+            <h2>Information On Parts</h2>
+            {selectedParts.map((part, index) => (
+              <div key={index} className="mb-4">
+                <h5>{part.name}</h5>
+                <p>
+                  Color:{" "}
+                  {part.color ? (
+                    <div
+                      className="color-display"
+                      style={{
+                        backgroundColor: part.color,
+                        width: "20px",
+                        height: "20px",
+                        display: "inline-block",
+                        marginLeft: "2rem",
+                      }}
+                    ></div>
+                  ) : (
+                    <span>N/A</span>
+                  )}
+                </p>
 
-            <p>Texture: {part.texture ? (
-              <p>
-                <img
-                  src={part.texture}
-                  alt="Selected Texture"
-                  style={{ maxWidth: "70px", maxHeight: "70px" ,display: "inline-block" }}
-                />
-              </p>
-            ) : (
-              <span>N/A</span>
-            )}</p>
-            
-            {index !== selectedParts.length - 1 && <hr />}
+                <p>
+                  Texture:{" "}
+                  {part.texture ? (
+                    <p>
+                      <img
+                        src={part.texture}
+                        alt="Selected Texture"
+                        style={{
+                          maxWidth: "70px",
+                          maxHeight: "70px",
+                          display: "inline-block",
+                        }}
+                      />
+                    </p>
+                  ) : (
+                    <span>N/A</span>
+                  )}
+                </p>
+
+                {index !== selectedParts.length - 1 && <hr />}
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
-    </div>
-  </div>
-  );
-})
-
+    );
+  }
+);
 
 export default Confirmation;
