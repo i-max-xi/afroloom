c947f56c086a03440c2f4c49ad388973926f2949
Austin <appiaha66@gmail.com>
2023-09-27 22:56:54 +0000

    male and female under 3$

--- src/Components/offers/arrays/rowOne.js
+++ src/Components/offers/arrays/rowOne.js
@@ -1,131 +1,138 @@
 import spinvid from "../../../Assets/vid/vid.mp4";
 
 export const rowOne = [
-    {
-      headTitle: "Men's Clothing under 3$",
-      array: [
-        {
-          // title: "Shirt",
-          imageUrl: "https://i.pinimg.com/originals/f7/1c/5c/f71c5c1e89dbb27a7e840b6fb60932eb.png",
-        },
-        {
-          // title: "Shorts",
-          imageUrl: "https://images.squarespace-cdn.com/content/v1/56064ca4e4b0c937d167b0b9/1488151203791-5Z4BJRKGQBAFH5W75GJE/44.png?format=1000w",
-        },
-        {
-          // title: "Trousers",
-          imageUrl: "https://www.pngall.com/wp-content/uploads/2016/09/Trouser-PNG-Picture.png",
-        },
-        {
-          // title: "Long Sleeves",
-          imageUrl: "https://clothbase.s3.amazonaws.com/uploads/148c7931-31f5-4c8b-82a4-e0427fea76f2/27-06-2017_cpcompany_longsleevepockettee_darkfoggrey_03cmts045a-000444g-968_mb_1.jpg",
-        },
-      ],
-      linkTo: "/",
-    },
-    {
-      title: "Design Your Own Products With Quality African Fabric",
-      videoUrl: spinvid,
-      linkTo: "/customize",
-    },
-    {
-      title: "Find the perfect gift",
-      imageUrl: "https://assets.mgimgs.com/mgimgs/rk/images/dp/wcm/202302/0008/mark-graham-x-steele-waterproof-medium-and-large-tote-set-t.jpg",
-      linkTo: "/category/Accessories",
-    },
-    
-    {
-      headTitle: "Women clothing under 3$",
-      array: [
-        {
-          // title: "Tshirt",
-          imageUrl: "https://images.squarespace-cdn.com/content/v1/56c69e66f8baf3bd6c33c52c/1690820831885-FKGBSS1DFCB2MVU70X0N/RED+VNECK_BLACK+LOGO.png",
-        },
-        {
-          // title: "Skirt",
-          imageUrl: "https://cdn.shopify.com/s/files/1/1639/2735/products/CircleSkirtinBlackPolkaDotStockFront_580x.jpg?v=1644861166",
-        },
-        {
-          // title: "Trouser",
-          imageUrl: "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1658768504-17020145_37177866_1000.jpg?crop=0.889xw:1xh;center,top&resize=980:*",
-        },
-        {
-          imageUrl: "https://www.uvskinz.com/cdn/shop/products/040352-White-1_308x308_crop_center.jpg?v=1683915837",
-        },
-      ]
-    },
-  ];
+  {
+    headTitle: "Men's Clothing under 3$",
+    array: [
+      {
+        // title: "Shirt",
+        imageUrl: require("../../../Assets/Offers/maleClothing/IMG-6847.JPG"),
+      },
+      {
+        // title: "Shorts",
+        imageUrl: require("../../../Assets/Offers/maleClothing/IMG-6853.JPG"),
+      },
+      {
+        // title: "Trousers",
+        imageUrl: require("../../../Assets/Offers/maleClothing/IMG-6846.WEBP"),
+      },
+      {
+        // title: "Long Sleeves",
+        imageUrl: require("../../../Assets/Offers/maleClothing/IMG-6858.JPG"),
+      },
+    ],
+    linkTo: "/",
+  },
+  {
+    title: "Design Your Own Products With Quality African Fabric",
+    videoUrl: spinvid,
+    linkTo: "/customize",
+  },
+  {
+    title: "Find the perfect gift",
+    imageUrl:
+      "https://assets.mgimgs.com/mgimgs/rk/images/dp/wcm/202302/0008/mark-graham-x-steele-waterproof-medium-and-large-tote-set-t.jpg",
+    linkTo: "/category/Accessories",
+  },
 
-  export const rowSix = [
-    
-    {
-      title: "Lowest Prices in 60 days",
-      imageUrl: "https://emaratshop.com/cdn/shop/products/ORANGEDSK_fec7dbb9-058d-4b0a-876a-b376604d6f91.png?v=1602629732",
-      linkTo: "/category/Accessories",
-    },
-    
-    {
-      title: 'Most Popular',
-      imageUrl: 'https://toppng.com/uploads/thumbnail/special-discount-sign-11552764408sqj0xsny4c.png',
-      linkTo: '/item-3-link',
-    },
-    {
-      title: "We make your thoughts",
-      videoUrl: spinvid,
-      // linkTo: "/category/Footwear",
-    },
-    {
-      headTitle: "New Products This Week",
-      array: [
-        {
-          title: "Pot",
-          imageUrl: "https://t4.ftcdn.net/jpg/02/55/26/07/360_F_255260766_F5NWLPmmuKqBHYd9rkOwVSplLPBlJxQP.jpg",
-        },
-        {
-          title: "Art Work",
-          imageUrl: "https://static.displate.com/280x392/displate/2019-04-04/d41aa25b6ecbf699df7a29209a263d90_8737498b15d0d4ae9a8d51ce07b623b2.jpg",
-        },
-        {
-          title: "Basket",
-          imageUrl: "https://i.pinimg.com/originals/84/a9/33/84a933b75922fb6c7676cf4aa244f2c4.png",
-        },
-      ]
-    },
-   
-  ];
+  {
+    headTitle: "Women clothing under 3$",
+    array: [
+      {
+        // title: "Tshirt",
+        imageUrl: require("../../../Assets/Offers/femaleClothing/IMG-6840.WEBP"),
+      },
+      {
+        // title: "Skirt",
+        imageUrl: require("../../../Assets/Offers/femaleClothing/IMG-6842.JPG"),
+      },
+      {
+        // title: "Trouser",
+        imageUrl: require("../../../Assets/Offers/femaleClothing/IMG-6838.JPG"),
+      },
+      {
+        imageUrl: require("../../../Assets/Offers/femaleClothing/IMG-6841.JPG"),
+      },
+    ],
+  },
+];
 
-  export const rowNine = [
-    {
-      headTitle: "Fashion Accessories Made Perfect",
-      array: [
-        {
-          title: "Watches",
-          imageUrl: "https://themesberg.com/docs/pixel-bootstrap/assets/img/shop/item-1.png",
-        },
-        {
-          title: "Bags",
-          imageUrl: "https://www.pngmart.com/files/22/Hobo-Bag-PNG-HD.png",
-        },
-        {
-          title: "Scarf",
-          imageUrl: "https://day-et.com/cdn/shop/products/WEB-3235490300-05029-1_1608914a-360c-4a71-9eb6-955c36d36253_1200x1200.png?v=1690599948",
-        },
-      ]
-    },
-    {
-      title: "Discover fashion trends",
-      imageUrl: "https://prod-images.fashionphile.com/thumb/d05b9222f2a254da3b751d3b127b5942/c7158f5539e1358d638e0bcf96804850.jpg",
-      linkTo: "/category/Accessories",
-    },
-    {
-      title: "We get it done like you design it",
-      videoUrl: spinvid,
-      // linkTo: "/category/Footwear",
-    },
-    {
-      title: 'Get Value For your Feet',
-      imageUrl: 'https://mawuafrica.com/cdn/shop/products/29_056f9282-89a8-43bc-8705-f51c7e1384ef.png?v=1678465703',
-      linkTo: '/item-3-link',
-    },
-   
-  ];
+export const rowSix = [
+  {
+    title: "Lowest Prices in 60 days",
+    imageUrl:
+      "https://emaratshop.com/cdn/shop/products/ORANGEDSK_fec7dbb9-058d-4b0a-876a-b376604d6f91.png?v=1602629732",
+    linkTo: "/category/Accessories",
+  },
+
+  {
+    title: "Most Popular",
+    imageUrl:
+      "https://toppng.com/uploads/thumbnail/special-discount-sign-11552764408sqj0xsny4c.png",
+    linkTo: "/item-3-link",
+  },
+  {
+    title: "We make your thoughts",
+    videoUrl: spinvid,
+    // linkTo: "/category/Footwear",
+  },
+  {
+    headTitle: "New Products This Week",
+    array: [
+      {
+        title: "Pot",
+        imageUrl:
+          "https://t4.ftcdn.net/jpg/02/55/26/07/360_F_255260766_F5NWLPmmuKqBHYd9rkOwVSplLPBlJxQP.jpg",
+      },
+      {
+        title: "Art Work",
+        imageUrl:
+          "https://static.displate.com/280x392/displate/2019-04-04/d41aa25b6ecbf699df7a29209a263d90_8737498b15d0d4ae9a8d51ce07b623b2.jpg",
+      },
+      {
+        title: "Basket",
+        imageUrl:
+          "https://i.pinimg.com/originals/84/a9/33/84a933b75922fb6c7676cf4aa244f2c4.png",
+      },
+    ],
+  },
+];
+
+export const rowNine = [
+  {
+    headTitle: "Fashion Accessories Made Perfect",
+    array: [
+      {
+        title: "Watches",
+        imageUrl:
+          "https://themesberg.com/docs/pixel-bootstrap/assets/img/shop/item-1.png",
+      },
+      {
+        title: "Bags",
+        imageUrl: "https://www.pngmart.com/files/22/Hobo-Bag-PNG-HD.png",
+      },
+      {
+        title: "Scarf",
+        imageUrl:
+          "https://day-et.com/cdn/shop/products/WEB-3235490300-05029-1_1608914a-360c-4a71-9eb6-955c36d36253_1200x1200.png?v=1690599948",
+      },
+    ],
+  },
+  {
+    title: "Discover fashion trends",
+    imageUrl:
+      "https://prod-images.fashionphile.com/thumb/d05b9222f2a254da3b751d3b127b5942/c7158f5539e1358d638e0bcf96804850.jpg",
+    linkTo: "/category/Accessories",
+  },
+  {
+    title: "We get it done like you design it",
+    videoUrl: spinvid,
+    // linkTo: "/category/Footwear",
+  },
+  {
+    title: "Get Value For your Feet",
+    imageUrl:
+      "https://mawuafrica.com/cdn/shop/products/29_056f9282-89a8-43bc-8705-f51c7e1384ef.png?v=1678465703",
+    linkTo: "/item-3-link",
+  },
+];
