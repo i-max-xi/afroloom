5daf55c96a34cde5dc2cda03d65d6e367445923c
Max <appiahmax4u@gmail.com>
2023-10-05 21:10:06 +0000

    start persist

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -210,18 +210,6 @@ const CardList = ({ currentPage, setCurrentPage, showNestedComponent }) => {
 
   const [itemsToDisplay, setItemsToDisplay] = useState(itemsToDisplayBank);
 
-  // const getProducts = async () => {
-  //   const data = await ProductsDataService.getAllProducts();
-  //   const fetchedProducts = data.docs.map((doc) => ({
-  //     ...doc.data(),
-  //     id: doc.id,
-  //   }));
-  //   dispatch(addProducts(fetchedProducts));
-  // };
-
-  // useEffect(() => {
-  //   getProducts();
-  // }, []);
 
   useEffect(() => {
     setItemsToDisplay(
