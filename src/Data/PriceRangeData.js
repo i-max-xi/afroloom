c58f2d9617cb50a84438dfdd704dcfafc88ca61a
Max <appiahmax4u@gmail.com>
2023-10-07 21:38:39 +0000

    new price range

--- src/Data/PriceRangeData.js
+++ src/Data/PriceRangeData.js
@@ -1,7 +1,7 @@
 export const getPriceRangeOptions = (currencySymbol, currencyFactor) => {
     return [
     //   { label: "All", value: "" },
-      { label: `Under ${currencySymbol}${currencyFactor * 20}`, value: 20 * currencyFactor },
+      { label: `Under ${currencySymbol}${currencyFactor * 10}`, value: 10 * currencyFactor },
       { label: `Under ${currencySymbol}${currencyFactor * 50}`, value: 50 * currencyFactor },
       { label: `${currencySymbol}${currencyFactor * 50} - ${currencySymbol}${currencyFactor * 100}`, value: 100 * currencyFactor },
       { label: `${currencySymbol}${currencyFactor * 100} - ${currencySymbol}${currencyFactor * 200}`, value: 200 * currencyFactor },
