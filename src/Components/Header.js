f7f86bcaf1da54e99e7ae25900d9a9ede9731bc7
Max <appiahmax4u@gmail.com>
2023-12-11 17:37:26 +0000

    centered all icon text

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -22,7 +22,7 @@ const Header = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
           >
             <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
           </svg>
-          <p className="d-flex flex-column justify-content-start">
+          <p className="d-flex flex-column justify-content-center">
             Fast & Secure Delivery
           </p>
         </li>
@@ -42,7 +42,7 @@ const Header = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
             <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
           </svg>
 
-          <p className="d-flex flex-column justify-content-start">
+          <p className="d-flex flex-column justify-content-center">
             Secured Payment System
             {/* <span style={{ fontSize: "0.8rem" }}>No question asked</span> */}
           </p>
@@ -60,7 +60,7 @@ const Header = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
             <path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z" />
           </svg>
 
-          <p className="d-flex flex-column justify-content-start">
+          <p className="d-flex flex-column justify-content-center">
             Money Back Guarantee
             {/* <span style={{ fontSize: "0.8rem" }}>Within 5 Business Days</span> */}
           </p>
@@ -77,7 +77,7 @@ const Header = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
             <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z" />
             <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
           </svg>
-          <p className="d-flex flex-column justify-content-start">
+          <p className="d-flex flex-column justify-content-center">
             24 X 7 Service
             <span style={{ fontSize: "0.8rem" }}>
               Online Service For Customer
