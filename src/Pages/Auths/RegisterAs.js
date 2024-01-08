d67b5e74797e64128c104d9fbe1b5a9ee4438727
Max <appiahmax4u@gmail.com>
2024-01-08 21:18:09 +0000

    register as

--- src/Pages/Auths/RegisterAs.js
+++ src/Pages/Auths/RegisterAs.js
@@ -6,26 +6,26 @@ const RegisterAs = () => {
   return (
     <div className="bg-white">
       <Nav />
-      <h4 className="mb-4 text-center">
+      <h4 className="mb-4 mt-2 text-center">
         <span className="text-warning">Register</span> As
       </h4>
-      <div className="container d-flex p-5">
+      <div className="container d-flex p-5 mb-5">
         <Link
           to="/buyer-signup"
-          className="w-50 text-decoration-none text-black m-5"
+          className="w-50 border rounded p-4 px-5 text-decoration-none text-black mx-2"
         >
-          <div className="card" data-aos="fade-in" data-aos-duration="1500">
-            <div className="card-body d-flex justify-content-center flex-column">
+          <div className="" data-aos="fade-in" data-aos-duration="1500">
+            <div className=" d-flex justify-content-center flex-column">
               <h5 className="text-center">Buyer</h5>
             </div>
           </div>
         </Link>
         <Link
           to="/supplier-signup"
-          className="w-50 text-decoration-none text-black m-5"
+          className="w-50 border rounded p-4 px-5 text-decoration-none text-black mx-2"
         >
-          <div className="card" data-aos="fade-in" data-aos-duration="1500">
-            <div className="card-body d-flex justify-content-center flex-column">
+          <div className=" " data-aos="fade-in" data-aos-duration="1500">
+            <div className="d-flex justify-content-center flex-column">
               <h5 className="text-center">Supplier</h5>
             </div>
           </div>
