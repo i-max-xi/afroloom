09cf1f281e178d58920535e67531a2cc31bd1ddf
Max <appiahmax4u@gmail.com>
2024-01-25 11:25:18 +0000

    fours

--- src/Components/offers/Fours.js
+++ src/Components/offers/Fours.js
@@ -8,23 +8,25 @@ const Fours = ({ items, headTitle, linkTo }) => {
       <h3 className="headTitle">{headTitle}</h3>
       <div>
         {items.length > 0 ? (
-          <div className="four-in-a-card">
-            {items.map((item, index) => (
-              <div className="item" key={index}>
-                <Link to={`/product/${item.id}`}>
-                  <img src={item.imageUrl} alt={item.title} width="80%" />
-                </Link>
-                {item.title && <h4>{item.title}</h4>}
-              </div>
-            ))}
-          </div>
+          <>
+            <div className="four-in-a-card">
+              {items.map((item, index) => (
+                <div className="item" key={index}>
+                  <Link to={`/product/${item.id}`}>
+                    <img src={item.imageUrl} alt={item.title} width="80%" />
+                  </Link>
+                  {item.title && <h4>{item.title}</h4>}
+                </div>
+              ))}
+            </div>
+            <Link to={linkTo} className="fours-link">
+              Shop Now
+            </Link>
+          </>
         ) : (
           <p>Currently Out Of Stock</p>
         )}
       </div>
-      <Link to={linkTo} className="fours-link">
-          Shop Now
-        </Link>
     </div>
   );
 };
