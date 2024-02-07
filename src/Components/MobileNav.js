a9513c4b0f68cbaa11c7d44a6702485f5611918a
Max <appiahmax4u@gmail.com>
2024-02-07 11:31:33 +0000

    clean up

--- src/Components/MobileNav.js
+++ src/Components/MobileNav.js
@@ -1,7 +1,6 @@
-import React, { useRef } from "react";
-import { Button } from "primereact/button";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link} from "react-router-dom";
 import { Sidebar } from "primereact/sidebar";
 
 import { Divider } from "primereact/divider";
