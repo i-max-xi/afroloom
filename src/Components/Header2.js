53232aee63a15cc845264e7c84d1e9567499082d
Max <appiahmax4u@gmail.com>
2024-03-24 15:06:42 +0000

    stable

--- src/Components/Header2.js
+++ src/Components/Header2.js
@@ -24,7 +24,7 @@ const Header2 = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
             <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
           </svg>
           <p className="d-flex flex-column justify-content-center">
-          <span className="fw-bolder">High Quality Assured</span>
+            <span className="fw-bolder">High Quality Assured</span>
           </p>
         </li>
         <li className="d-flex more-info border-right">
@@ -44,7 +44,7 @@ const Header2 = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
           </svg>
 
           <p className="d-flex flex-column justify-content-center">
-          <span className="fw-bolder">Secured Payment System</span>
+            <span className="fw-bolder">Secured Payment System</span>
             {/* <span style={{ fontSize: "0.8rem" }}> Lorem ipsum, dolor sit</span> */}
           </p>
         </li>
@@ -64,8 +64,7 @@ const Header2 = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
           <p className="d-flex flex-column justify-content-center">
             <span className="fw-bolder">Free Returns</span>
             <span style={{ fontSize: "0.8rem" }}>
-              {" "}
-              Free return if products are damaged.
+              Free return within 48hours of purchase
             </span>
           </p>
         </li>
@@ -75,16 +74,22 @@ const Header2 = ({ bgColor, Color, Padding, Icon1, Icon2, Icon3, Icon4 }) => {
             // width="16"
             // height="16"
             fill="currentColor"
-            className="bi bi-hand-thumbs-up more-info-icon"
+            class="bi bi-cash-coin more-info-icon"
             viewBox="0 0 16 16"
           >
-            <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
+            <path
+              fill-rule="evenodd"
+              d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8m5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0"
+            />
+            <path d="M9.438 11.944c.047.596.518 1.06 1.363 1.116v.44h.375v-.443c.875-.061 1.386-.529 1.386-1.207 0-.618-.39-.936-1.09-1.1l-.296-.07v-1.2c.376.043.614.248.671.532h.658c-.047-.575-.54-1.024-1.329-1.073V8.5h-.375v.45c-.747.073-1.255.522-1.255 1.158 0 .562.378.92 1.007 1.066l.248.061v1.272c-.384-.058-.639-.27-.696-.563h-.668zm1.36-1.354c-.369-.085-.569-.26-.569-.522 0-.294.216-.514.572-.578v1.1zm.432.746c.449.104.655.272.655.569 0 .339-.257.571-.709.614v-1.195z" />
+            <path d="M1 0a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h4.083q.088-.517.258-1H3a2 2 0 0 0-2-2V3a2 2 0 0 0 2-2h10a2 2 0 0 0 2 2v3.528c.38.34.717.728 1 1.154V1a1 1 0 0 0-1-1z" />
+            <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 6 6 0 0 1 3.13-1.567" />
           </svg>
           <p className="d-flex flex-column justify-content-center">
-          <span className="fw-bolder">High End Delivery For All</span>
-            {/* <span style={{ fontSize: "0.8rem" }}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            </span> */}
+            <span className="fw-bolder">45% Down Payment</span>
+            <span style={{ fontSize: "0.8rem" }}>
+              Pay just 45% now, top up upon delivery.
+            </span>
           </p>
         </li>
       </ul>
