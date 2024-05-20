95ce7fbda7e949f71cb7b118034ce92032cf497b
i-max-xi <appiahmax4u@gmail.com>
2024-05-20 18:55:46 +0000

    remove videos

--- src/Pages/Customize/Configurator/TakeTour.js
+++ src/Pages/Customize/Configurator/TakeTour.js
@@ -1,48 +1,50 @@
 import { Dialog } from "primereact/dialog";
 import React, { useMemo } from "react";
 
-import maleVid from "../../../Assets/Tutorials/male.mp4";
-import femaleVid from "../../../Assets/Tutorials/female.mp4";
-import hairVid from "../../../Assets/Tutorials/hair.mp4";
-import bikiniVid from "../../../Assets/Tutorials/bikini.mp4";
-import bangleVid from "../../../Assets/Tutorials/bangles.mp4";
-import sashVid from "../../../Assets/Tutorials/sash.mp4";
-import earringVid from "../../../Assets/Tutorials/earring.mp4";
+// import maleVid from "../../../Assets/Tutorials/male.mp4";
+// import femaleVid from "../../../Assets/Tutorials/female.mp4";
+// import hairVid from "../../../Assets/Tutorials/hair.mp4";
+// import bikiniVid from "../../../Assets/Tutorials/bikini.mp4";
+// import bangleVid from "../../../Assets/Tutorials/bangles.mp4";
+// import sashVid from "../../../Assets/Tutorials/sash.mp4";
+// import earringVid from "../../../Assets/Tutorials/earring.mp4";
 
 const TakeTour = ({ isOpen, onClose, type }) => {
-  const videoUrl = useMemo(() => {
-    if (type === "male") {
-      return maleVid;
-    }
+  // const videoUrl = useMemo(() => {
+  //   if (type === "male") {
+  //     return maleVid;
+  //   }
 
-    if (type === "female") {
-      return femaleVid;
-    }
+  //   if (type === "female") {
+  //     return femaleVid;
+  //   }
 
-    if (type === "bikini") {
-      return bikiniVid;
-    }
+  //   if (type === "bikini") {
+  //     return bikiniVid;
+  //   }
 
-    if (type === "hair") {
-      return hairVid;
-    }
+  //   if (type === "hair") {
+  //     return hairVid;
+  //   }
 
-    if (type === "nails") {
-      return;
-    }
+  //   if (type === "nails") {
+  //     return;
+  //   }
 
-    if (type === "bangle") {
-      return bangleVid;
-    }
+  //   if (type === "bangle") {
+  //     return bangleVid;
+  //   }
 
-    if (type === "earring") {
-      return earringVid;
-    }
+  //   if (type === "earring") {
+  //     return earringVid;
+  //   }
 
-    if (type === "sash") {
-      return sashVid;
-    }
-  }, [type]);
+  //   if (type === "sash") {
+  //     return sashVid;
+  //   }
+  // }, [type]);
+
+  const videoUrl = "";
 
   return (
     <Dialog
